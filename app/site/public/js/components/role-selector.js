22ecbfcfc0ce4680cd1c4d42e0ca0c75a07dc4b9
Sergey Ryzhov <serega.rf@gmail.com>
2016-07-22 08:06:00 +0300

    role-selector

--- app/site/public/js/components/role-selector.js
+++ app/site/public/js/components/role-selector.js
@@ -11,26 +11,33 @@ define([
 
   return function (params) {
     var nextComponent = 'game-player';
-    function join(role) {
-      if (role == 'manager') {
-        nextComponent = 'game-mngr';
-      }
-      if (role == 'view') {
-        nextComponent = 'game-view';
-      }
-      if (role.substring(0, 4) == 'team') {
-        params.team(Number(role.substring(4)));
-      }
 
-      params.view(nextComponent);
+    var password = ko.observable('');
+    
+    var passSubscription = password.subscribe(join);
+
+    function join(value) {
+      if (!value || value.length < 4)
+        return;  
+
+      value == '2705' && params.view('game-mngr');
+      value == '0507' && params.view('game-view');
+      value == '2907' && params.view('game-player') || params.team(1);
+      value == '0512' && params.view('game-player') || params.team(1);
+    }
+
+    function dispose(params) {
+      passSubscription.dispose();
     }
 
     return {
-      join: join,
-      team1Text: params.players[0].title,
-      team2Text: params.players[1].title,
-      managerText: 'Ведущий',
-      viewText: 'Наблюдатель',
+      password:password,
+      // join: join,
+      // team1Text: params.players[0].title,
+      // team2Text: params.players[1].title,
+      // managerText: 'Ведущий',
+      // viewText: 'Наблюдатель',
+      dispose:dispose
     };
   };
 });
